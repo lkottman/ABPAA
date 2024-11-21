bbccebc54618edd80d7884e9891b77ea1bf368f7
olivermaska <143597833+olivermaska@users.noreply.github.com>
2024-11-21 15:31:59 +0100

    Additional Information app.py

--- app.py
+++ app.py
@@ -14,6 +14,28 @@ def show_form():
 def index():
     if request.method == 'POST':
 
+ <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Finance</title>
+</head>
+<body>
+    <form>
+        <label for="ticker">Ticker:</label>
+        <input type="text" id="ticker" name="ticker" placeholder="z.B. AAPL" required><br><br>
+ 
+        <label for="start_year">Start Year:</label>
+        <input type="text" id="start_year" name="start_year" placeholder="z.B. 2020" required><br><br>
+ 
+        <label for="end_year">End Year5:</label>
+        <input type="text" id="end_year" name="end_year" placeholder="z.B. 2021" required><br><br>
+ 
+        <button type="submit">get</button>
+    </form>
+</body>
+</html>
+            
         #input
         ticker = request.form['ticker']
         start_year = request.form['start_year']
@@ -23,7 +45,34 @@ def index():
         stock = yf.Ticker(ticker)
         data = stock.history(start=f"{start_year}-01-01", end=f"{end_year}-12-31")
     
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Finance Data Results</title>
+</head>
+<body class="bg-light">
+    <div class="container d-flex flex-column justify-content-center align-items-center vh-100">
+        <h1 class="text-center mb-4">Results for {{ ticker }} from </h1>
+        <h2 class="text-center mb-4">Basisinformation</h2>
+        <ul>
+            <li>Company name: {{ info['longName'] }}</li>
+            <li>Market capitalization: {{ info['marketCap'] }}</li>
+            <li>Sector: {{ info['sector'] }}</li>
+            <li>Country: {{ info['country'] }}</li>
+            <li>Website: {{ info['website'] }}</a></li>
+        </ul>
+        <h2 class="text-center mb-4">Financial data for {{ ticker }} from </h1>
+        <div class="table-responsive">
+            {{ data |safe }}
+        </div>
+        <a href="/">Zurück zur Eingabe</a>
+    </div>
+</body>
+</html>
 
         ##sector = info.get('sector', 'Unknown')
 
